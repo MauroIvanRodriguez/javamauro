3d7344effe9c54f6e2d87eac0906cf9dba236d29
Mauro Ivan Rodríguez <mauroivanr1@gmail.com>
2020-10-25 17:44:43 -0300

    first exercise done

---
+++ index1.js
@@ -0,0 +1,14 @@
+// Variables
+var num1 = 2;
+var num2 = 4;
+var nombre1 ="mauro"
+var nombre2 = "ivan";
+
+//Operaciones y nuevas variables
+var sumanum= num1 + num2;
+var ambosNombres = nombre1 + nombre2;
+var sumaCaracteres = nombre1.length  + nombre2.length ;
+console.log(sumanum);
+console.log(ambosNombres);
+console.log(sumaCaracteres);
+
