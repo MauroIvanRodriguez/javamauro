d706a052c182befe8cb727d259290579682683fe
Mauro Ivan Rodríguez <mauroivanr1@gmail.com>
2020-10-25 23:42:34 -0300

    Fourth exercise done and other changes

---
+++ index4.js
@@ -0,0 +1,30 @@
+// Item a)
+var num = Math.random();
+console.log(num);
+if(num>=0.5){
+    alert("Greater than 0,5");
+}else {
+    alert("Lower than 0,5");
+}
+
+
+// Item b)
+
+var age = 19;
+console.log(age);
+
+if(age <= 2){
+    alert("Bebe");
+}else if(age <= 12){
+    alert("Nino");
+}else if(age <= 19){
+    alert("Adolecente")
+}else if(age <= 30){
+    alert("Joven")
+}else if(age <= 60){
+    alert("Adulto");
+}else if(age <= 75){
+    alert("Adulto mayor");
+}else {
+    alert("Anciano");
+}
